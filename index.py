083a9eea22e9e060df8ef4a79100d3211923ae94
Swaka500 <derrickswaka910@gmail.com>
2025-03-25 11:26:39 +0300

    Complited week 2 python assignment

--- index.py
+++ index.py
@@ -1,1 +1,24 @@
+# An empty list
+my_list = []
 
+# Appended elements
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# 15 inserted at the second position
+my_list.insert(1, 15)
+
+# List extended with another
+my_list.extend([50, 60, 70])
+
+# Remove the last element
+my_list.pop()
+
+# List sorted in ascending order
+my_list.sort()
+
+# Find and print the index of value 30
+index_30 = my_list.index(30)
+print("Index of 30:", index_30)
